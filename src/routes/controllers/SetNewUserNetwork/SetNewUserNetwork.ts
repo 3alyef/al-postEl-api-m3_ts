051ef499107bf6258d8b9d187c07fc1914c08134
3alyef <3alyef.dev@gmail.com>
2024-04-14 17:27:40 -0300

    correcao de tipos

--- src/routes/controllers/SetNewUserNetwork/SetNewUserNetwork.ts
+++ src/routes/controllers/SetNewUserNetwork/SetNewUserNetwork.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import { CreateNewUserNetwork } from "../../../services/Services";
-import { userNetworkRequest } from "../../../interfaces/userNetworkSetNew.interface";
+import { networkRequest } from "../../../interfaces/userNetworkSetNew.interface";
 
 class SetNewUserNetworkController {
-    public async newUserNetworkPost(req: Request<{body: userNetworkRequest}>, res: Response){     
+    public async newUserNetworkPost(req: Request<{body: networkRequest}>, res: Response){     
         new CreateNewUserNetwork().initialize(req, res);
     }
 }
