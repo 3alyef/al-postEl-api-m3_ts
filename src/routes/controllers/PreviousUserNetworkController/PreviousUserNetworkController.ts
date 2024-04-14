52d279b331b43db00175bd659d8697d7c304c9bd
3alyef <3alyef.dev@gmail.com>
2024-04-14 16:47:23 -0300

    update

--- src/routes/controllers/PreviousUserNetworkController/PreviousUserNetworkController.ts
+++ src/routes/controllers/PreviousUserNetworkController/PreviousUserNetworkController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { GetPrevUserNetwork } from "../../../services/Services";
-import { userNetworkRequest } from "../../../interfaces/userNetworkSetNew.interface";
+
 class PreviousUserNetworkController {
     public async previousUserNetworkPost(req: Request<{body: userNetworkGetRequest}>, res: Response){
         new GetPrevUserNetwork().initialize(req, res)
