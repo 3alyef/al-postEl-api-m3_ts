d33b35f1e45bdc41449c871a5fe68d0dd9c8c6b0
3alyef <3alyef.dev@gmail.com>
2024-06-04 22:56:51 -0300

    update group

--- src/db/models/MessageGroupModel/MessageGroupModel.ts
+++ src/db/models/MessageGroupModel/MessageGroupModel.ts
@@ -3,40 +3,9 @@ import { mongoose } from "../../DB";
 const { Schema, model } = mongoose;
 const NewMessageGroupSchema = new Schema({
     fromUser: { type: String, required: true },
-    deletedTo: { 
-        type: Schema.Types.Mixed, 
-        required: true,
-        validate: {
-            validator: function(v: string | MapConstructor) {
-                if (v === "none") return true;
-                if (v instanceof Map) {
-                    for (let val of v.values()) {
-                        if (!["justTo", "justFrom", "all"].includes(val)) return false;
-                    }
-                    return true;
-                }
-                return false;
-            },
-            message: (props: {value: string}) => `${props.value} is not a valid deletedTo value!`
-        }
-    },
+    deletedTo: String,
     toUsers: { type: [String], required: true },
-    viewStatus: { 
-        type: Schema.Types.Mixed, 
-        validate: {
-            validator: function(v: string | MapConstructor) {
-                if (v === "onServer") return true;
-                if (v instanceof Map) {
-                    for (let val of v.values()) {
-                        if (!["delivered", "seen"].includes(val)) return false;
-                    }
-                    return true;
-                }
-                return false;
-            },
-            message: (props: {value: string}) => `${props.value} is not a valid viewStatus!`
-        }
-    },
+    viewStatus: String,
     toGroup: { type: String, required: true },
     message: { type: String, required: true },
     createdIn: { type: String, required: true }
--- src/services/CreateNewGroupMsg/CreateNewGroupMsg.service.ts
+++ src/services/CreateNewGroupMsg/CreateNewGroupMsg.service.ts
@@ -7,6 +7,11 @@ class CreateNewGroupMsg {
         try {
             const {fromUser, deletedTo, viewStatus, toGroup, message, createdIn, toUsers}: msgsGroupRequest = req.body;
             //console.log("here =>>",fromUser, toGroup, message, createdIn);
+            console.log('viewStatus', viewStatus);
+
+            //const adjustedDeletedTo = deletedTo === "none" ? "none" : new Map(deletedTo);
+            //const adjustedViewStatus = viewStatus === "onServer" ? "onServer" : new Map(viewStatus);
+            //console.log('///', adjustedDeletedTo, adjustedViewStatus)
             const newMsg = await this.registrerNewMsg({fromUser, deletedTo, viewStatus, toGroup, message, createdIn, toUsers});
             res.status(200).json(newMsg).end();
         } catch(error){
@@ -17,25 +22,27 @@ class CreateNewGroupMsg {
     }
 
     private async registrerNewMsg({fromUser, deletedTo, viewStatus, toGroup, message, createdIn, toUsers}: msgsGroupRequest){
-        
+        const viewStatusV = JSON.stringify(viewStatus);
+        const deletedToV = JSON.stringify(deletedTo)
         try {
             const newGroupMessage = new messageGroupModel (
                 {
                     createdIn,
-                    deletedTo,
+                    deletedTo: deletedToV,
                     fromUser,
                     message,
                     toGroup,
                     toUsers,
-                    viewStatus
+                    viewStatus: viewStatusV
                 }
             )
 
+           
             await newGroupMessage.save();
             return newGroupMessage;
 
         } catch (error) {
-            throw new Error("Ocorreu um erro ao registrar uma nova mensagem no database.");
+            throw new Error("Ocorreu um erro ao registrar uma nova mensagem no database." + error);
 
         }
     }
